e3395209e5d3571d273e2f396b5b95180960b935
kiminsoo114 <kiminsu114@hanmail.net>
2022-04-05 09:07:59 +0900

    동적쿼리

---
+++ Chapter7-MVCProject(Basic)/src/main/java/com/sist/dao/ReplyBoardDAO.java
@@ -0,0 +1,106 @@
+package com.sist.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import com.sist.vo.*;
+import com.sist.mapper.*;
+@Repository
+public class ReplyBoardDAO {
+   // 구현된 매퍼를 스프링으로부터 받아 온다 
+   @Autowired
+   private ReplyBoardMapper mapper;// 구현된 클래스의 주소값을 자동주입 
+   public List<ReplyBoardVO> replyBoardListData(Map map)
+   {
+	   return mapper.replyBoardListData(map);
+   }
+   public int replyBoardTotalPage()
+   {
+	   return mapper.replyBoardTotalPage();
+   }
+   public int replyBoardCount()
+   {
+	   return mapper.replyBoardCount();
+   }
+   public void replyBoardInsert(ReplyBoardVO vo)
+   {
+	   mapper.replyBoardInsert(vo);
+   }
+   public ReplyBoardVO replyBoardDetailData(int no)
+   {
+	   mapper.hitIncrement(no);// 조회수 증가 
+	   return mapper.replyBoardDetailData(no);
+   }
+   public ReplyBoardVO replyBoardUpdateData(int no)
+   {
+	   return mapper.replyBoardDetailData(no);
+   }
+   
+   public boolean replyBoardUpdate(ReplyBoardVO vo)
+   {
+	   boolean bCheck=false;
+	   // 비밀번호 검색
+	   String db_pwd=mapper.replyBoardGetPassword(vo.getNo());
+	   if(db_pwd.equals(vo.getPwd()))
+	   {
+		   bCheck=true;
+		   // 실제 수정 
+		   mapper.replyBoardUpdate(vo);
+	   }
+	   else
+	   {
+		   bCheck=false;
+	   }
+	   return bCheck;
+   }
+   // 답변 
+   @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+   public void replyBoardReplyInsert(int pno,ReplyBoardVO vo)
+   {
+	   ReplyBoardVO pvo=mapper.replyBoardParentInfoData(pno);
+	   mapper.replyGroupStepIncrement(pvo);
+	   vo.setGroup_id(pvo.getGroup_id());
+	   vo.setGroup_step(pvo.getGroup_step()+1);
+	   vo.setGroup_tab(pvo.getGroup_tab()+1);
+	   mapper.replyBoardReplyInsert(vo);
+	   mapper.replyBoardDepthIncrement(pno);
+   }
+   
+   @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+   public boolean replyBoardDelete(int no,String pwd)
+   {
+	   boolean bCheck=false;
+	   // 비밀번호 가지고 오기 
+	   String db_pwd=mapper.replyBoardGetPassword(no);
+	   if(db_pwd.equals(pwd))
+	   {
+		   bCheck=true;
+		   //정보 
+		   ReplyBoardVO vo=mapper.replyBoardGetRootDepth(no);
+		   if(vo.getDepth()==0)// 댓글이 없는 게시물
+		   {
+			   mapper.replyBoardDelete(no);
+		   }
+		   else // 댓글이 있는 게시물 
+		   {
+			   mapper.replySubContUpdate(no);
+		   }
+		   
+		   mapper.replyBoardDepthDecrement(no);
+	   }
+	   return bCheck;
+   }
+   public List<ReplyBoardVO> replyBoardFindData(Map map)
+   {
+	   return mapper.replyBoardFindData(map);
+   }
+}
+
+
+
+
+
+
---
+++ Chapter7-MVCProject(Basic)/src/main/java/com/sist/mapper/ReplyBoardMapper.java
@@ -0,0 +1,170 @@
+package com.sist.mapper;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.sist.vo.*;
+
+import lombok.Delegate;
+public interface ReplyBoardMapper {
+   // 기능 
+   // 1. 목록출력 => 페이징 
+   @Select("SELECT no,subject,name,"
+		  +"TO_CHAR(regdate,'YYYY-MM-DD') as dbday,hit,group_tab,num "
+		  +"FROM (SELECT no,subject,name,regdate,hit,group_tab,rownum as num "
+		  +"FROM (SELECT no,subject,name,regdate,hit,group_tab "
+		  +"FROM spring_replyboard "
+		  +"ORDER BY group_id DESC,group_step ASC)) "
+		  +"WHERE num BETWEEN #{start} AND #{end}")
+   public List<ReplyBoardVO> replyBoardListData(Map map);
+   // 1-1. 총페이지 
+   @Select("SELECT CEIL(COUNT(*)/10.0) FROM spring_replyboard")
+   public int replyBoardTotalPage();
+   
+   // 1-2. 출력 순서 
+   @Select("SELECT COUNT(*) FROM spring_replyboard")
+   public int replyBoardCount();
+   
+   // 2. 새글 올리기 
+   @Insert("INSERT INTO spring_replyboard(no,name,subject,content,"
+		  +"pwd,group_id) VALUES("
+		  +"sr_no_seq.nextval,#{name},#{subject},"
+		  +"#{content},#{pwd},"
+		  +"(SELECT NVL(MAX(group_id)+1,1) FROM spring_replyboard))")
+   public void replyBoardInsert(ReplyBoardVO vo);
+   // 3. 상세보기 
+   @Select("SELECT no,name,subject,content,"
+		  +"TO_CHAR(regdate,'YYYY-MM-DD') as dbday,hit "
+		  +"FROM spring_replyboard "
+		  +"WHERE no=#{no}")
+   public ReplyBoardVO replyBoardDetailData(int no);
+   // 3-1. 조회수 증가 
+   @Update("UPDATE spring_replyBoard SET "
+		  +"hit=hit+1 "
+		  +"WHERE no=#{no}")
+   public void hitIncrement(int no);
+   // 4. 답변 : SQL문장 여러개 사용 => 트랜잭션 (XML , Annotation)
+   // 4-1 : no에 해당되는 데이터 읽기 (group_id , group_step,group_tab)
+   @Select("SELECT group_id,group_step,group_tab "
+		  +"FROM spring_replyboard "
+		  +"WHERE no=#{no}")
+   // 상위 게시물  => pno 
+   public ReplyBoardVO replyBoardParentInfoData(int no);
+   // 4-2 : group_step => 증가  => UPDATE
+   @Update("UPDATE spring_replyBoard SET "
+		  +"group_step=group_step+1 "
+		  +"WHERE group_id=#{group_id} AND group_step>#{group_step}")
+   public void replyGroupStepIncrement(ReplyBoardVO vo);
+   // 4-3 : 실제 insert  => INSERT
+   @Insert("INSERT INTO spring_replyBoard(no,name,subject,content,pwd,"
+		  +"group_id,group_step,group_tab,root) VALUES("
+		  +"sr_no_seq.nextval,#{name},#{subject},#{content},"
+		  +"#{pwd},#{group_id},#{group_step},"
+		  +"#{group_tab},#{root})")
+   public void replyBoardReplyInsert(ReplyBoardVO vo);
+   // 4-4 : depth증가  => UPDATE
+   @Update("UPDATE spring_replyBoard SET "
+		  +"depth=depth+1 "
+		  +"WHERE no=#{no}")
+   public void replyBoardDepthIncrement(int no);
+   // 비절차 언어 => 에러발생 => 밑에 있는 문장을 수행한다 (단점) 
+   // 일괄 처리  => 4개전체 성공 (COMMIT) , 4개중에 1개라도 실패(Rollback)
+   // 트랜잭션 프로그램 (금융권 , 공기업 => 트랜잭션 처리가 거의 등장)
+   /*
+    *    try
+    *    {
+    *        getConnection()
+    *        SELECT
+    *        UPDATE
+    *        INSERT
+    *        UPDATE
+    *        commit()
+    *    }catch(Exception ex)
+    *    {
+    *        rollback()
+    *    }
+    *    finally
+    *    {
+    *       disConnection()
+    *    }
+    *    
+    *    @Transactional
+    */
+   // 5. 삭제 : SQL문장 여러개 사용 => 트랜잭션
+   // 5-1. 비밀번호 검색 
+   // 5-1-1 .  root,depth가지고 오는 
+   @Select("SELECT root,depth FROM spring_replyboard "
+		  +"WHERE no=#{no}")
+   public ReplyBoardVO replyBoardGetRootDepth(int no);
+   // 5-2. 삭제 / 수정 
+   @Delete("DELETE FROM spring_replyboard "
+		  +"WHERE no=#{no}")
+   public void replyBoardDelete(int no);
+   @Update("UPDATE spring_replyboard SET "
+		  +"subject='관리자가 삭제한 게시물입니다.',"
+		  +"content='관리자가 삭제한 게시물입니다.' "
+		  +"WHERE no=#{no}")
+   public void replySubContUpdate(int no);
+   // 5-3. depth감소
+   @Update("UPDATE spring_replyboard SET "
+		  +"depth=depth-1 "
+		  +"WHERE no=#{no}")
+   public void replyBoardDepthDecrement(int no);
+   // 6. 수정 
+   @Select("SELECT pwd FROM spring_replyboard "
+		  +"WHERE no=#{no}")
+   public String replyBoardGetPassword(int no);
+   
+   @Update("UPDATE spring_replyboard SET "
+		  +"name=#{name},subject=#{subject},"
+		  +"content=#{content} "
+		  +"WHERE no=#{no}")
+   public void replyBoardUpdate(ReplyBoardVO vo);
+   // 7. 찾기 : MyBatis동적 쿼리 작성방법 (XML , Annotation)
+   /*
+    *    WHERE 
+    *      name LIKE '%'||#{ss}||'%'
+    *      subject LIKE '%'||#{ss}||'%'
+    *      content LIKE '%'||#{ss}||'%'
+    *      
+    *      추가  prefix , suffix 
+    *      제거 prefixOverrides  suffixOverrides
+    */
+   @Select("<script>"
+		  +"SELECT no,subject,name,"
+		  +"TO_CHAR(regdate,'YYYY-MM-DD') as dbday,hit "
+		  +"FROM spring_replyboard "
+		  +"WHERE "
+		  +"<trim prefixOverrides='OR'>"
+		  +"<foreach collection='fsArr' item='fd'>"
+		  +"<trim prefix='OR'>"
+		  +"<choose>"
+		  +"<when test=\"fd=='N'.toString()\">"
+		  +"name LIKE '%'||#{ss}||'%'"
+		  +"</when>"
+		  +"<when test=\"fd=='S'.toString()\">"
+		  +"subject LIKE '%'||#{ss}||'%'"
+		  +"</when>"
+		  +"<when test=\"fd=='C'.toString()\">"
+		  +"content LIKE '%'||#{ss}||'%'"
+		  +"</when>"
+		  +"</choose>"
+		  +"</trim>"
+		  +"</foreach>"
+		  +"</trim>"
+		  +"</script>"
+		  )
+   public List<ReplyBoardVO> replyBoardFindData(Map map);
+}
+
+
+
+
+
+
+
+
+
